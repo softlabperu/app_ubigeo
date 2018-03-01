1f67a00c9628ff7f8045f41967755f49252261fd
Jason Lazo <jlazolock@softlabperu.com>
2018-03-01 17:30:31 -0500

    - Eliminar migraciones - Reparar comando para que no borre ubigeos existentes - Exponer api

--- app_ubigeo/urls.py
+++ app_ubigeo/urls.py
@@ -9,5 +9,5 @@ router.register(r'provincia', ProvinciaViewSet, base_name='provincia')
 router.register(r'distrito', DistritoViewSet, base_name='distrito')
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^app_ubigeo/', include(router.urls)),
 ]
