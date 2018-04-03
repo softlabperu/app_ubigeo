15741e81a2b1ca850c583f6d3aa79a62e8748d47
jvallina <jvallina@softlabperu.com>
2018-04-03 16:28:20 -0500

    - Ignorar distributables

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
-from setuptools import find_packages, setup
+import setuptools
+# from setuptools import find_packages, setup
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
@@ -7,10 +8,10 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-setup(
+setuptools.setup(
     name='django-ubigeo',
-    version='0.1.2 rc',
-    packages=find_packages(),
+    version='0.1.3',
+    packages=setuptools.find_packages(),
     include_package_data=True,
     license='BSD License',  # example license
     description='A simple Django app to validate peru ubigeo',
