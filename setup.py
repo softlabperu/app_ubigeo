aca22566c2e9a569441a014f57e330ccd0d240f5
jvallina <jvallina@softlabperu.com>
2018-04-04 13:51:04 -0500

    - Ignorar distributables

--- setup.py
+++ setup.py
@@ -8,9 +8,11 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+version=os.environ.get('MODULE_VERSION')
+
 setuptools.setup(
     name='django-ubigeo',
-    version='0.1.3',
+    version=version,
     packages=setuptools.find_packages(),
     include_package_data=True,
     license='BSD License',  # example license
