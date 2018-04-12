b35d5fe86781aa421f5fb0792d6115c82b4ce006
jvallina <jvallina@softlabperu.com>
2018-04-12 13:57:54 -0500

    Get version from environment variales

--- setup.py
+++ setup.py
@@ -9,8 +9,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-version = os.environ.get('MODULE_VERSION')
-version = '5.0.1'
+version = os.environ.get('MODULE_VERSION', '0.0.0')
 
 setuptools.setup(
     name='django-ubigeo',
